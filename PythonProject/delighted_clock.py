f7b38ec038ea85dfd574ed6f916f642ae1f82a70
CaballeroFF <CaballeroFF@users.noreply.github.com>
2018-05-06 15:04:48 -0700

    phase one functionality update
    added all routes to python RESTful app and linked it to android project. thoroughly tested each function

--- PythonProject/delighted_clock.py
+++ PythonProject/delighted_clock.py
@@ -2,68 +2,68 @@ import sys
 import time
 import datetime
 from pygame import mixer
-import delight_leds as dl
+#import delight_leds as dl
 import threading
 
 
 def parse_duration(dstr):
-    time_type = ''
-    t_index = dstr.index('T')
-    for i, elem in enumerate(dstr):
-        if elem != 'P' and elem != 'T':
-            if elem.isnumeric():
-                time_type += elem
-            if elem.isalpha() and len(dstr) - 1 != i:
-                if elem == 'M' and i >= t_index:
-                    time_type += elem.lower() + ' '
-                else:
-                    time_type += elem + ' '
-            if len(dstr) - 1 == i:
-                if elem == 'M' and i >= t_index:
-                    time_type += elem.lower()
-                else:
-                    time_type += elem
-    duration = time_type.split(' ')
-    return duration
+	time_type = ''
+	t_index = dstr.index('T')
+	for i, elem in enumerate(dstr):
+		if elem != 'P' and elem != 'T':
+			if elem.isnumeric():
+				time_type += elem
+			if elem.isalpha() and len(dstr) - 1 != i:
+				if elem == 'M' and i >= t_index:
+					time_type += elem.lower() + ' '
+				else:
+					time_type += elem + ' '
+			if len(dstr) - 1 == i:
+				if elem == 'M' and i >= t_index:
+					time_type += elem.lower()
+				else:
+					time_type += elem
+	duration = time_type.split(' ')
+	return duration
 
 
 def duration_to_minute(dstr):
-    dlist = parse_duration(dstr)
-    year = 0
-    month = 0
-    week = 0
-    day = 0
-    hour = 0
-    minute = 0
-    for i, elem in enumerate(dlist):
-        if 'Y' in elem:
-            year = int(elem[:-1]) * 525600
-        if 'M' in elem:
-            month = int(elem[:-1]) * 43800
-        if 'W' in elem:
-            week = int(elem[:-1]) * 10078
-        if 'D' in elem:
-            day = int(elem[:-1]) * 1440
-        if 'H' in elem:
-            hour = int(elem[:-1]) * 60
-        if 'm' in elem:
-            minute = int(elem[:-1])
-    minutes = year + month + week + day + hour + minute
-    return minutes
+	dlist = parse_duration(dstr)
+	year = 0
+	month = 0
+	week = 0
+	day = 0
+	hour = 0
+	minute = 0
+	for i, elem in enumerate(dlist):
+		if 'Y' in elem:
+			year = int(elem[:-1]) * 525600
+		if 'M' in elem:
+			month = int(elem[:-1]) * 43800
+		if 'W' in elem:
+			week = int(elem[:-1]) * 10078
+		if 'D' in elem:
+			day = int(elem[:-1]) * 1440
+		if 'H' in elem:
+			hour = int(elem[:-1]) * 60
+		if 'm' in elem:
+			minute = int(elem[:-1])
+	minutes = year + month + week + day + hour + minute
+	return minutes
 
 
 def alarm_duration(aduration):
-    ctime = time.localtime()
-    dt = datetime.datetime.fromtimestamp(time.mktime(ctime))
-    c = dt + datetime.timedelta(minutes=duration_to_minute(aduration))
-    adate = str(c.date())
-    atime = str(c.hour) + ':' + str(c.minute)
+	ctime = time.localtime()
+	dt = datetime.datetime.fromtimestamp(time.mktime(ctime))
+	c = dt + datetime.timedelta(minutes=duration_to_minute(aduration))
+	adate = str(c.date())
+	atime = str(c.hour) + ':' + str(c.minute)
 
-    #set_alarm(atime, adate)
-    return atime, adate
+	# set_alarm(atime, adate)
+	return atime, adate
 
 
-#sa = set alarm //// sd = set date
+# sa = set alarm //// sd = set date
 def set_alarm(salarm, sdate):
 	mixer.init()
 	mixer.music.load('alarm.mp3')
@@ -108,7 +108,8 @@ def set_alarm(salarm, sdate):
 
 	try:
 		print("Sleeping for " + str(minutes) + unit_word)
-		while not (ctime.tm_hour == int(timelist[0]) and ctime.tm_min == int(timelist[1]) and ctime.tm_mday == int(datelist[2])):
+		while not (ctime.tm_hour == int(timelist[0]) and ctime.tm_min == int(timelist[1]) and ctime.tm_mday == int(
+				datelist[2])):
 			if change != minutes:
 				change = minutes
 				print("Sleeping for " + str(minutes) + unit_word)
@@ -122,51 +123,54 @@ def set_alarm(salarm, sdate):
 			minutes = time_in_min
 			if minutes < 0:
 				minutes = minutes + 1440
-		
+
 		print("Wake up")
-		dl.build()
-		#SLEEP controls the speed of the leds
-		#MSTART indicates when the beeping starts
-		dl.start(SLEEP= 0.1, MSTART= 25)
-		print('in loop')
-		
-		stop = False
-		while dl.ir_approach():
-			print(chr(7))
-			dl.one_time()
-			time.sleep(.01)
-			#print('btn is ', dl.btn_press())
-			stop = dl.btn_press()
-			if stop:
-				break
-		dl.clean_up()
-		
-		if not stop:
-			snooze_min = int(timelist[1]) + 1 #snooze time
-			snooze_hr = int(timelist[0])
-			if snooze_min > 59:
-				snooze_min -= 59 
-				snooze_hr += 1
-			if snooze_hr > 23:
-				snooze_hr -= 24
-			print(salarm)
-			print(str(snooze_hr) + ':' + str(snooze_min))
-			new_time = str(snooze_hr) + ':' + str(snooze_min)
-			set_alarm(new_time, sdate)
-		print('stopped')
-		
-		
-			
 	except KeyboardInterrupt:
-		print("Interrupted by user")
-		mixer.music.stop()
-		dl.clean_up()
-		sys.exit(1)
+		pass
+	# 	dl.build()
+	# 	# SLEEP controls the speed of the leds
+	# 	# MSTART indicates when the beeping starts
+	# 	dl.start(SLEEP=0.1, MSTART=25)
+	# 	print('in loop')
+	#
+	# 	stop = False
+	# 	while dl.ir_approach():
+	# 		print(chr(7))
+	# 		dl.one_time()
+	# 		time.sleep(.01)
+	# 		# print('btn is ', dl.btn_press())
+	# 		stop = dl.btn_press()
+	# 		if stop:
+	# 			break
+	# 	dl.clean_up()
+	#
+	# 	if not stop:
+	# 		snooze_min = int(timelist[1]) + 1  # snooze time
+	# 		snooze_hr = int(timelist[0])
+	# 		if snooze_min > 59:
+	# 			snooze_min -= 59
+	# 			snooze_hr += 1
+	# 		if snooze_hr > 23:
+	# 			snooze_hr -= 24
+	# 		print(salarm)
+	# 		print(str(snooze_hr) + ':' + str(snooze_min))
+	# 		new_time = str(snooze_hr) + ':' + str(snooze_min)
+	# 		set_alarm(new_time, sdate)
+	# 	print('stopped')
+	#
+	#
+	#
+	# except KeyboardInterrupt:
+	# 	print("Interrupted by user")
+	# 	mixer.music.stop()
+	# 	dl.clean_up()
+	# 	sys.exit(1)
 
 	return -1
-# EOF
-#set_alarm('15:03','2018-03-12')
 
-#debug
-#set_alarm(str(time.localtime().tm_hour) + ':' + str(time.localtime().tm_min),'2018-03-17')
-#print(alarm_duration(u'PT3M'))
+# EOF
+# set_alarm('15:03','2018-03-12')
+
+# debug
+# set_alarm(str(time.localtime().tm_hour) + ':' + str(time.localtime().tm_min),'2018-03-17')
+# print(alarm_duration(u'PT3M'))
