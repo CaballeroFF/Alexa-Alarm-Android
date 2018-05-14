bc92d15f4a48a30b01c503c8fef4f9281c6631c7
Carlos <32400995+EsparzaCarlos@users.noreply.github.com>
2018-05-13 23:37:22 -0700

    Revert "Added stopwatch with bugs"
    This reverts commit 58a9a8203b3e96068c8b9c0782ca643440d7758f.

--- AndroidProject/DeLighted/app/src/main/java/com/example/caballero/delighted/TabFourFragment.java
+++ AndroidProject/DeLighted/app/src/main/java/com/example/caballero/delighted/TabFourFragment.java
@@ -1,82 +1,19 @@
 package com.example.caballero.delighted;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class TabFourFragment extends android.support.v4.app.Fragment{
     private static final String TAG = "TabFourFragment";
-
-    private TextView timeViwer,txtValue;
-    Handler customHandler = new Handler();
-    private LinearLayout txtcontainer;
-
-    long startTime=0L, timeInMilliseconds=0L, timeSwapBuff=0L,updateTime=0L;
-
-    Runnable updateTimeThread = new Runnable() {
-        @Override
-        public void run() {
-            timeInMilliseconds = SystemClock.uptimeMillis()-startTime;
-            updateTime = timeSwapBuff+timeInMilliseconds;
-            int secs= (int)(updateTime/1000);
-            int mins = secs/60;
-            secs%=60;
-            int millisec = (int) (updateTime%1000);
-            timeViwer.setText(""+String.format("%02d",mins)+":"+
-                    String.format("%02d",secs)+":"+
-                    String.format("%03d",millisec));
-            customHandler.postDelayed(this,0);
-        }
-    };
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab4_frag,container,false);
 
-        timeViwer =  view.findViewById(R.id.stopwatchTimer);
-        txtcontainer = view.findViewById(R.id.txtcontainer);
-
-        Button btnLap = view.findViewById(R.id.lapBtn);
-        Button btnPause = view.findViewById(R.id.pauseBtn);
-        Button btnStart = view.findViewById(R.id.startBtn);
-
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startTime = SystemClock.uptimeMillis();
-
-                customHandler.postDelayed(updateTimeThread, 0);
-            }
-        });
-
-        btnPause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                timeSwapBuff=timeInMilliseconds;
-                customHandler.removeCallbacks(updateTimeThread);
-            }
-        });
-
-        btnLap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LayoutInflater inflaterTime = (LayoutInflater)getActivity().getBaseContext().getSystemService(getContext().LAYOUT_INFLATER_SERVICE);
-                View addview = inflaterTime.inflate(R.layout.laptimes,null);
-                txtValue = view.findViewById(R.id.lapsTime);
-                txtValue.setText("hello");
-//                txtcontainer.addView(addview);
-            }
-        });
-
         return view;
     }
 }
-
--- AndroidProject/DeLighted/app/src/main/java/com/example/caballero/delighted/TabTwoFragment.java
+++ AndroidProject/DeLighted/app/src/main/java/com/example/caballero/delighted/TabTwoFragment.java
@@ -1,70 +1,150 @@
 package com.example.caballero.delighted;
 
-import android.animation.Animator;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Typeface;
-import android.net.Uri;
 import android.os.BatteryManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.text.format.Time;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Switch;
-import android.widget.TextClock;
-import android.widget.TextView;
 
-import java.net.URL;
 import java.util.Timer;
 
 public class TabTwoFragment extends android.support.v4.app.Fragment{
     private static final String TAG = "TabTwoFragment";
 
-    private TextClock tClock,dClock;
-    private TextView dayOfWeek;
-    private Button worldTime;
-
-    String url = "https://www.worldtimeserver.com/";
+    Time mTime;
+    Handler handler;
+    Runnable r;
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public drawingView onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab2_frag,container,false);
 
-        tClock = (TextClock) getActivity().findViewById(R.id.TextClock);
-        dClock = (TextClock) getActivity().findViewById(R.id.TextDate);
-        dayOfWeek = (TextView) getActivity().findViewById(R.id.TextDayOfWeek);
-        Button worldTime = view.findViewById(R.id.worldTimeBttn);
+        mTime = new Time();
 
-        worldTime.setOnClickListener(new View.OnClickListener() {
+        r = new Runnable() {
             @Override
-            public void onClick(View view) {
-                goToUrl();
+            public void run() {
+                mTime.setToNow();
+                drawingView dv = new drawingView(getActivity(),
+                        mTime.hour,mTime.minute,mTime.second,mTime.weekDay,mTime.monthDay, getBatteryLevel());
+                handler.postDelayed(r,1000);
             }
-        });
+        };
+        handler = new Handler();
 
-        return view;
+        mTime.setToNow();
+        drawingView dv = new drawingView(getActivity(),
+                mTime.hour,mTime.minute,mTime.second,mTime.weekDay,mTime.monthDay, getBatteryLevel());
+        handler.postDelayed(r, 1000);
+
+        return dv;
+
+//        return view;
     }
 
-    public void goToSu (View view) {
-        goToUrl ();
+
+    public float getBatteryLevel(){
+        Intent batterIntent = getActivity().registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        int Level = batterIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batterIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        if (Level == -1 || scale == -1){
+            return 50.0f;
+        }
+
+        return ((float) Level/ (float) scale) * 100.0f;
     }
 
-    private void goToUrl () {
-        Intent viewIntent =
-                new Intent("android.intent.action.VIEW",
-                        Uri.parse(url));
-        startActivity(viewIntent);
+    public class drawingView extends View {
+
+        Paint mBackgroundPaint, mTextPaint;
+        Typeface tf;
+
+        int hours, minutes, seconds, weekday, Date;
+        float battery;
+
+        public drawingView(Context context, int hours, int minutes, int seconds, int weekday, int Date, float battery){
+            super(context);
+
+            tf = Typeface.createFromAsset(getActivity().getAssets(), "Inconsolata-Regular.ttf");
+
+            mBackgroundPaint = new Paint();
+            mBackgroundPaint.setColor(getActivity().getColor(R.color.colorPrimary));
+
+            mTextPaint = new Paint();
+            mTextPaint.setColor(getActivity().getColor(R.color.text));
+            mTextPaint.setAntiAlias(true);
+            mTextPaint.setTextAlign(Paint.Align.CENTER);
+            mTextPaint.setTypeface(tf);
+
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+            this.weekday = weekday;
+            this.Date = Date;
+            this.battery = battery;
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas){
+            super.onDraw(canvas);
+
+            float width = canvas.getWidth();
+            float height = canvas.getHeight();
+
+            canvas.drawRect(0,0,width,height,mBackgroundPaint);
+
+            float centerX = width/2f;
+            float centerY = height/2f;
+
+            int cur_hour = hours;
+
+            String cur_ampm = "AM";
+            if(cur_hour == 0){
+                cur_hour = 12;
+            }
+            if(cur_hour > 12){
+                cur_hour = cur_hour - 12;
+                cur_ampm = "PM";
+            }
+
+            String text = String.format("%02d:%02d:%02d", cur_hour,minutes,seconds);
+
+            String days_of_week = "";
+            switch(weekday){
+                case 1: days_of_week = "MON";
+                case 2: days_of_week = "TUE";
+                case 3: days_of_week = "WED";
+                case 4: days_of_week = "THU";
+                case 5: days_of_week = "FRI";
+                case 6: days_of_week = "SAT";
+                case 7: days_of_week = "SUN";
+            }
+            String text2 = String.format("DATE: %s %d", days_of_week,Date);
+            String batteryLevel = "BATTERY" + (int) battery + "%";
+
+            canvas.drawText("00 00 00",centerX,centerY,mBackgroundPaint);
+
+            mTextPaint.setColor(getActivity().getColor(R.color.text));
+            mTextPaint.setTextSize(getResources().getDimension(R.dimen.text_size));
+            canvas.drawText(text,centerX,centerY, mTextPaint);
+
+            mTextPaint.setColor(getActivity().getColor(R.color.text));
+            mTextPaint.setTextSize(getResources().getDimension(R.dimen.text_size_small));
+            canvas.drawText(batteryLevel + " "+  text2,
+                    centerX,
+                    centerY + getResources().getDimension(R.dimen.text_size_small),
+                    mTextPaint);
+        }
     }
 }
